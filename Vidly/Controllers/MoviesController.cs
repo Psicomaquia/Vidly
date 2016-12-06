64b7f1c9e0bdebd7a7e34cd880ca025719b15242
psicomaquia <im.ibbuk@gmail.com>
2016-12-06 13:21:10 -0600

    Add Movies and details

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -5,25 +5,42 @@ using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
 using Vidly.ViewModels;
+using System.Data.Entity;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         public ViewResult Index()
         {
-            var movies = GetMovies();
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
+
             return View(movies);
         }
 
-        private IEnumerable<Movie> GetMovies()
+        public ActionResult Details(int id)
         {
-            return new List<Movie>
-            {
-                new Movie {Id = 1, Name ="Shrek"},
-                new Movie {Id = 2, Name = "Wall-e"}
-            };
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            return View(movie);
+
         }
+
 
         // GET: Movies/Random
         public ActionResult Random()
@@ -31,19 +48,17 @@ namespace Vidly.Controllers
             var movie = new Movie() { Name = "Shrek!" };
             var customers = new List<Customer>
             {
-                new Customer{Name ="Customer 1"},
-                new Customer{Name ="Customer 2"}
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
             };
-            var ViewModel = new RandomMovieViewModel
+
+            var viewModel = new RandomMovieViewModel
             {
                 Movie = movie,
                 Customers = customers
             };
-           
-            return View(ViewModel);
+
+            return View(viewModel);
         }
-
     }
-
-    
 }
